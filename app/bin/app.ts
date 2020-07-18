dea6068934b4712e465e4ed3ac0cfcd7b403ce75
Venkat Sundaram <venkatachalapatee@gmail.com>
2020-07-18 18:22:39 -0500

    adding app stack

--- app/bin/app.ts
+++ app/bin/app.ts
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { AppStack } from '../lib/app-stack';
+import {AppStack} from '../lib/app-stack';
 
 const app = new cdk.App();
-new AppStack(app, 'AppStack');
+new AppStack(app, 'AppStack', {
+    env: {region: "us-east-1"},
+    tags: {'billing': '123'}
+
+});
--- app/lib/app-stack.ts
+++ app/lib/app-stack.ts
@@ -1,9 +1,44 @@
 import * as cdk from '@aws-cdk/core';
+import * as ec2 from '@aws-cdk/aws-ec2';
+import * as ecs from '@aws-cdk/aws-ecs';
+import {SubnetType} from "@aws-cdk/aws-ec2/lib/vpc";
 
 export class AppStack extends cdk.Stack {
+
+  get stackName(): string {
+    return 'App';
+  }
+
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+
     // The code that defines your stack goes here
+    const vpc = new ec2.Vpc(this, 'VPC', {
+      maxAzs: 3,
+      subnetConfiguration: [
+        {
+          name: 'public',
+          subnetType: ec2.SubnetType.PUBLIC,
+        },
+        {
+          name: 'private',
+          subnetType: ec2.SubnetType.PRIVATE,
+        },
+      ]
+    });
+
+    const cluster = new ecs.Cluster(this, 'Cluster', {
+      vpc,
+    });
+
+    cluster.addCapacity('DefaultAutoScalingGroupCapacity', {
+      instanceType: new ec2.InstanceType("t2.xlarge"),
+      desiredCapacity: 3,
+      vpcSubnets: {
+        subnetType: SubnetType.PRIVATE,
+      }
+    });
+
   }
 }
